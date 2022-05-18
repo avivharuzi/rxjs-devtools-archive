9718192575c26f7b28e7806a3d3020ae99d22dca
Aviv Haruzi <avivharuzi@gmail.com>
2022-05-18 21:48:42 +0300

    refactor: 💡 update demo app

--- apps/demo/src/app/app.component.ts
+++ apps/demo/src/app/app.component.ts
@@ -1,4 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+import { Subject } from 'rxjs';
+
+import { rxJSDevToolsTag } from '@rxjs-devtools/core';
 
 @Component({
   selector: 'demo-root',
@@ -6,4 +11,23 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent {
+  sub = new Subject<string>();
+
+  sub$ = this.sub.asObservable().pipe(rxJSDevToolsTag('sub'));
+
+  constructor(private readonly httpClient: HttpClient) {
+    this.sub.subscribe();
+  }
+
+  next() {
+    this.sub.next(`random: ${Math.floor(Math.random() * 9999) + 1}`);
+  }
+
+  subscribe() {
+    this.httpClient
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .pipe(rxJSDevToolsTag('posts'))
+      .subscribe();
+  }
+}
