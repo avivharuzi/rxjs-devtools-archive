f495a0157d7aa8cd9c7f468177891bae1ab8cb3c
Aviv Haruzi <avivharuzi@gmail.com>
2022-05-18 18:25:23 +0300

    feat: 🎸 add background and content scripts as apps

---
+++ apps/scripts/background/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'scripts-background',
+  preset: '../../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  transform: {
+    '^.+\\.[tj]s$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/apps/scripts/background',
+};
